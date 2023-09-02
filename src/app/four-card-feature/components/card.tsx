f570f2276d1eae96fe531838809bf2ba00b54ec8
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-09-02 20:44:30 -0300

    feat(four-card-feature): adds mobile layout with assets

---
+++ src/app/four-card-feature/components/card.tsx
@@ -0,0 +1,29 @@
+import './card.css';
+import Image from 'next/image';
+
+type CardProps = {
+  title: string,
+  content: string,
+  color: string,
+  imageName: string,
+}
+
+export default function Card({ title, content, color, imageName }: CardProps) {
+  return (
+    <article className='card' style={{ borderTopColor: color }}>
+      <h3 className='card__title'>
+        { title }
+      </h3>
+      <p className='card__content'>
+        { content }
+      </p>
+      <Image
+        className='card__image'
+        src={`/four-card-feature/${imageName}.svg`}
+        width="58"
+        height="58"
+        alt={imageName}
+      />
+    </article>
+  )
+}
--- src/app/four-card-feature/page.tsx
+++ src/app/four-card-feature/page.tsx
@@ -1,9 +1,71 @@
+import './page.css';
+import { Poppins } from 'next/font/google';
+import type { Metadata } from 'next';
+import Card from './components/card';
+
+export const metadata: Metadata = {
+  title: 'Frontend Mentor | Four card feature section',
+  description: 'Four card feature section',
+}
+
+const poppins = Poppins({
+  weight: ['200', '400', '600'],
+  subsets: ['latin']
+});
+
+const cards = [
+  {
+    color: 'hsl(180, 62%, 55%)',
+    title: 'Supervisor',
+    content: 'Monitors activity to identify project roadblocks',
+    imageName: 'icon-supervisor'
+  },
+  {
+    color: 'hsl(0, 78%, 62%)',
+    title: 'Team Builder',
+    content: 'Scans our talent network to create the optimal team for your project',
+    imageName: 'icon-team-builder'
+  },
+  {
+    color: 'hsl(34, 97%, 64%)',
+    title: 'Karma',
+    content: 'Regularly evaluates our talent to ensure quality',
+    imageName: 'icon-karma'
+  },
+  {
+    color: 'hsl(212, 86%, 64%)',
+    title: 'Calculator',
+    content: 'Uses data from past projects to provide better delivery estimates',
+    imageName: 'icon-calculator'
+  }
+];
 
 export default function FourCardFeature() {
   return (
-    <main>
-      <h1>Hello four card feature</h1>
-      <h2>Starting the page</h2>
+    <main className={`container ${poppins.className}`}>
+      <h2 className='title'>
+        Reliable, efficient delivery
+      </h2>
+      <h1 className='title weight600'>
+        Powered by Technology
+      </h1>
+      <p className='content'>
+        Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful
+      </p>
+      <div className='cardContainer'>
+        {
+          cards.map((card, i) => (
+              <Card
+                key={i}
+                title={card.title}
+                content={card.content}
+                color={card.color}
+                imageName={card.imageName}
+              />
+            )
+          )
+        }
+      </div>
     </main>
   );
 }
