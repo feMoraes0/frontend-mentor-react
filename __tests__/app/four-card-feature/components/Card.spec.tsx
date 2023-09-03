fece234fe2c18be43ceb1131dbe8e99dcafbc7ca
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-09-03 18:08:51 -0300

    test(four-card-feature): creates card unit test file

---
+++ __tests__/app/four-card-feature/components/Card.spec.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import Card from "../../../../src/app/four-card-feature/components/card";
+import "@testing-library/jest-dom";
+
+describe("Given Card component rendered", () => {
+  const props = {
+    title: "any title",
+    content: "any content",
+    color: "any color",
+    imageName: "any_image",
+  }
+
+  beforeEach(() => {
+    render(<Card { ...props } / >);
+  });
+
+  it("Then should render a principal text with received props title", () => {
+    expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(props.title);
+  });
+
+  it("Then should render a paragraph text with received props content", () => {
+    expect(screen.getByText(props.content)).toBeInTheDocument();
+  });
+
+  it("Then should render an image with source defined", () => {
+    expect(screen.getByRole("img")).toHaveAttribute("src", `/four-card-feature/${props.imageName}.svg`);
+  });
+});
+
