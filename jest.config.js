ec8b289753cd8739cef9eef4ab9ab8ede27c979c
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2023-11-29 19:46:22 -0300

    chore: adds babel config and updates jest config

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,11 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
   testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
 };
 
 module.exports = createJestConfig(customJestConfig);
